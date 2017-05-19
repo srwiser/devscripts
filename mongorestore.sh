660ca7d9b6fe566e03e14494d34d86d09d56c79e
srwiser <srwiser@gmail.com>
2017-05-19 15:58:05 +0530

    updated mongorestore

--- mongorestore.sh
+++ mongorestore.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-DBNAME="vymo-lms"
-BACKUP_PATH="/home/vagrant/app30832600"
-#for indexfile in /home/vagrant/app30832600/*.json; do
-#	echo "Restoring indexes from $indexfile"
-#	/bin/sed -i 's/, "background" : true//g' $indexfile
-#	/usr/bin/mongorestore -d $DBNAME -u vymo-super-user -p AAAAB3NzaC1yc2EAAAADAQABAAACAQCswC6hMNTersMSW4nOdgLh8Gxw9bu0BFvyW3oRgzo1 --authenticationDatabase admin $indexfile
-#done
+DBNAME="database name"
+BACKUP_PATH="/backup"
+DB_ADMIN_USER="database admin"
+DB_ADMIN_PASSWORD="admin password"
 	
 for datafile in $BACKUP_PATH/*.bson; do
 	echo "Restoring $datafile"
-        /usr/bin/mongorestore -d $DBNAME -u vymo-super-user -p AAAAB3NzaC1yc2EAAAADAQABAAACAQCswC6hMNTersMSW4nOdgLh8Gxw9bu0BFvyW3oRgzo1 --authenticationDatabase admin $datafile
+        /usr/bin/mongorestore -d $DBNAME -u $DB_ADMIN_USER -p $DB_ADMIN_PASSWORD --authenticationDatabase admin $datafile
 done
