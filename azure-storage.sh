6640723f09b92d2ab3c7f8cc08938ca21c1d47ed
srwiser <srwiser@gmail.com>
2017-06-07 17:39:42 +0530

    improved script

--- azure-storage.sh
+++ azure-storage.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 # A simple Azure Storage example script
 
-export AZURE_STORAGE_ACCOUNT="Acc_name"
-export AZURE_STORAGE_ACCESS_KEY="Acc_key"
+export AZURE_STORAGE_ACCOUNT="storage_acc_name"
+export AZURE_STORAGE_ACCESS_KEY="storage_acc_key"
 
-export container_name="Con_name"
+export container_name="blob_container_name"
 echo "Creating the container..."
-
 az storage container create --name $container_name
 
-FILES="List_of_files"  ## you can read this from a directory
+ext="csv"
+data_path="/data/dump/csv/$container_name"
+FILES=`ls $data_path/ | grep -F ".$ext"`
 echo "$FILES\n"
+
 if [ ! -z "$FILES" -a "$FILES" != " "  ]; then
 
         for filename in $FILES; do
                 echo "uploading $filename"
                 export blob_name="$filename"
-                export file_to_upload="/data/path/$filename"
+                export file_to_upload="$data_path/$filename"
+                export destination_file="$data_path/$filename"
                 echo "Uploading the file..."
                 az storage blob upload --container-name $container_name --file $file_to_upload --name $blob_name
+                echo "Downloading the file..."
+                #az storage blob download --container-name $container_name --name $blob_name --file $destination_file --output table
         done
 else
         echo "No such files found"
 fi
 
-export destination_file=
-
 echo "Listing the blobs..."
 az storage blob list --container-name $container_name --output table
-
-echo "Downloading the file..."
-az storage blob download --container-name $container_name --name $blob_name --file $destination_file --output table
 
 echo "Done"
