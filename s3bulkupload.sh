d1c946e7e65de35b32745b005691e5ad9eb44213
srwiser <srwiser@gmail.com>
2017-08-22 14:27:46 +0530

    file exist check in s3 bulk upload

--- s3bulkupload.sh
+++ s3bulkupload.sh
@@ -9,8 +9,14 @@ TOTAL_FILES=`ls $LOCAL_DIR | wc -l`
 
 for filename in $LOCAL_DIR/*.$FILE_EXT; do
         cd $LOCAL_DIR
-        echo "Uploading $filename ..."
-        s3cmd put $filename s3://$BUCKET_NAME/$BUCKET_PATH
+	s3cmd ls s3://$BUCKET_NAME/$BUCKET_PATH | grep -i "$filename"
+        SUCCESS=`echo $?`
+        if [ "$SUCCESS" == "0" ];then
+           echo "File already exist"
+        else
+           echo "Uploading $filename ..."
+           s3cmd put $filename s3://$BUCKET_NAME/$BUCKET_PATH
+        fi
 done
 
 TOTAL_FILES_S3=`s3cmd ls s3://$BUCKET_NAME/$BUCKET_PATH | wc -l`
